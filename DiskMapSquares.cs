0fb1b36de9c46690db58644a48b2fbba97541fa2
markostruna <struna@ANKH.hermes.si>
2015-03-16 12:15:13 +0100

    Missing files added.

---
+++ DiskMapSquares.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TDefragWpf.Library.Common;
+
+namespace TDefragWpf
+{
+    class DiskMapSquares1
+    {
+        public Int64 ChildPosition { get; set; }
+
+        eClusterState clusterState;
+    }
+}
